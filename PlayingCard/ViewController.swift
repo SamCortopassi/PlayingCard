2d4e7ec42ffc313c6eb9e81c7c9194d5860fc2a5
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2018-12-06 17:55:00 -0600

    checking

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -10,4 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var deck = PlayingCardDeck()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        for _ in 1...10 {
+            if let card = deck.draw() {
+                print ("\(card)")
+            }
+        }
+    }
+    
 }
