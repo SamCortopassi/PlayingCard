aaf462334425d3c78b1bf68f979e75e097fe3d32
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2018-12-06 17:23:32 -0600

    set up deck

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -10,21 +10,4 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        
-        
-        /**** DO STUFF HERE ****/
-        
-        
-        /**** END: DO STUFF HERE ****/
-        
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 }
