0dd9e581ea3ea77a6b23c738edf659ef67101e69
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2018-12-06 16:57:51 -0600

    clean

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -9,62 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-    
-    
-    //
-    //  ViewController.swift
-    //  Playing Card
-    //
-    //  Created by Samantha Cortopassi on 12/6/18.
-    //  Copyright © 2018 Samantha Cortopassi. All rights reserved.
-    //
-    
-    import UIKit
-    
-    class ViewController: UIViewController {
         
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            // Do any additional setup after loading the view, typically from a nib.
-            
-            /**** DO STUFF HERE ****/
-            
-            var number: Int = 0
-            for number in 1...50 {
-                
-                if number % 3 == 0, number % 5 == 0 {
-                    print("\(number) FizzBuzz")
-                }
-                else if number % 5 == 0 {
-                    print("\(number) buzz")
-                }
-                else if number % 3 == 0 {
-                    print("\(number) Fizz")
-                }
-                else if number >= 1 {
-                    print("\(number)")}
-            }
-            
-            func didReceiveMemoryWarning() {
-                super.didReceiveMemoryWarning()
-                // Dispose of any resources that can be recreated.
-            }
-            
-        }
+        
+        
+        /**** DO STUFF HERE ****/
+        
+        
+        /**** END: DO STUFF HERE ****/
+        
     }
     
-
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
