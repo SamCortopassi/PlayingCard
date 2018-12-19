a90c9dd58d5f738ba2a5e14cd43dfc069979e6c5
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2018-12-19 11:34:23 -0600

    draw arc

--- PlayingCard/PlayingCardView.swift
+++ PlayingCard/PlayingCardView.swift
@@ -11,7 +11,12 @@ import UIKit
 class PlayingCardView: UIView {
     override func draw(_ rect: CGRect) {
         if let context = UIGraphicsGetCurrentContext() {
-            context.addArc(center: <#T##CGPoint#>, radius: <#T##CGFloat#>, startAngle: <#T##CGFloat#>, endAngle: <#T##CGFloat#>, clockwise: <#T##Bool#>)
+            context.addArc(center: CGPoint(x:bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+            context.setLineWidth(5.0)
+            UIColor.green.setFill()
+            UIColor.red.setStroke()
+            context.strokePath()
+            context.fillPath()
         }
 
     }
