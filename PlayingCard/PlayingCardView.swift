a635a3e84ef930a0a4df400d7703b89a9cb54cba
Samantha Cortopassi <s.b.cortopassi@gmail.com>
2018-12-19 11:30:24 -0600

    add draw rec

---
+++ PlayingCard/PlayingCardView.swift
@@ -0,0 +1,20 @@
+//
+//  PlayingCardView.swift
+//  PlayingCard
+//
+//  Created by Samantha Cortopassi on 12/19/18.
+//  Copyright © 2018 Samantha Cortopassi. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+    override func draw(_ rect: CGRect) {
+        if let context = UIGraphicsGetCurrentContext() {
+            context.addArc(center: <#T##CGPoint#>, radius: <#T##CGFloat#>, startAngle: <#T##CGFloat#>, endAngle: <#T##CGFloat#>, clockwise: <#T##Bool#>)
+        }
+
+    }
+    
+
+}
